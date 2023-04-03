975a3faa2427131825adbcc36bb41ad9bf733251
天逸 <albert.xht@alibaba-inc.com>
2023-04-04 00:09:04 +0800

    add self-instruct

--- self_instruct/generate_instruction_only.py
+++ self_instruct/generate_instruction_only.py
@@ -37,7 +37,7 @@ openai.api_key = random_key
 
 def encode_prompt(prompt_instructions):
     """Encode multiple prompt instructions into a single string."""
-    prompt = open("/home/htxu91/chatgpt/self_instruct/prompt_cn_risk.txt").read() + "\n"
+    prompt = open("/home/htxu91/chatgpt/self_instruct/prompt_cn_extend.txt").read() + "\n"
     print(prompt, '===prompt===')
     for idx, task_dict in enumerate(prompt_instructions):
         instruction = task_dict["instruction"]
