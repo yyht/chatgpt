ae8ebe4df819202f49818152ea045fe24fa4cf6d
天逸 <albert.xht@alibaba-inc.com>
2023-06-08 12:02:54 +0800

    add risk-reverse

--- query_risk/safety_prompte_risk_reverse.py
+++ query_risk/safety_prompte_risk_reverse.py
@@ -70,14 +70,12 @@ def reverse(data_list, chunks, chunk_key, output_file_):
         for idx in tqdm(chunks):
             item = data_list[idx]
 
-            print(item)
-
             if not item['is_response_0_safe']:
                 source = 'response_0'
             elif not item['is_response_1_safe']:
                 source = 'response_1'
 
-            prompt = meta_prompt.format(item['prompt'], item[source])
+            prompt = meta_prompt.format(item['query'], item[source])
 
             random_key = np.random.choice(key_list)
             openai.api_key = random_key
