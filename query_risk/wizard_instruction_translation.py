7d481dc356cf5e1607b00ffe8623f7703b292607
天逸 <albert.xht@alibaba-inc.com>
2023-06-19 11:07:09 +0800

    add risk-reverse

--- query_risk/wizard_instruction_translation.py
+++ query_risk/wizard_instruction_translation.py
@@ -14,7 +14,6 @@ with open('/home/htxu91/keys.txt', 'r') as frobj:
         key_list.append(line.strip())
 
 meta_prompt = """
-I want you to act as an Chinese translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in Chinese. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level Chinese words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. 
 将输入文本翻译成中文:
 输入文本: 
 {}
